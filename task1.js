98793608a1980146e45ff5a215181df7f909bd8d
Hreniks <sbigor09@gmail.com>
2021-12-17 14:02:12 +0300

    ended first task

--- task1.js
+++ task1.js
@@ -1,19 +1,42 @@
-function createDebounceFunction(cb, ms) {
-    let timeout;
-    const startTime = new Date().getTime();
-    return function () {
+
+
+Array.prototype.myFilter = function (cb, thisArg) {
+    if (this == null) throw new Error('Cant iterate over undefined or null');
+    if (typeof cb !== 'function') throw new Error('callback is not a function');
+    let context = this;
+    let result = [];
+
+    let O = Object(this);
+    let length = O.length;
+
+
+    if (thisArg) {
+        context = thisArg;
+    }
+
+    
+    for (let i = 0; i < length; i++) {
+        if (i in O) {
+            if (cb.call(context, this[i], i, O)){
+                result.push(this[i]);
+            }
+        }
         
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(cb, ms);
-        const time = new Date().getTime() - startTime;
-       console.log((time / 1000).toFixed(1));
-    };
-}
+    }
+    
+    return result;
 
-const log100 = () => console.log(100);
-const debounceLog100 = createDebounceFunction(log100, 1000);
+    // while (i < this.length) {
+    //     if (i in this) {
+    //         cb.call(context, this[i], i, this);
+    //     }
+    //     i++;
+    // }
 
-debounceLog100();
 
-setTimeout(debounceLog100, 200); // так как задержка в 1000мс и новый вызов этой же функции происходит через 200 миллисекунд, то таймер запускается заново
-setTimeout(debounceLog100, 400); // снова сбрасываем таймер ещё через 200 миллисекунд
+};
+
+let arr = [1, 2, 3, 4, 5];
+
+let second = arr.myFilter((item,index,array) => item > 3);
+console.log(second);
