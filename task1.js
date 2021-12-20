b290cd9955b449afd97ef3f86d45d0fc70c1f771
Hreniks <sbigor09@gmail.com>
2021-12-20 17:36:26 +0300

    first commit

--- task1.js
+++ task1.js
@@ -1,42 +1,172 @@
+const solution = document.querySelector('.calc-solution');
+const operatorText = document.querySelector('.calc-operator');
+const buttons = document.querySelectorAll('.calc-button');
+let number1 = '';
+let number2 = '';
+let result = '';
+let isOperationClicked = false;
+let operator = '';
 
-
-Array.prototype.myFilter = function (cb, thisArg) {
-    if (this == null) throw new Error('Cant iterate over undefined or null');
-    if (typeof cb !== 'function') throw new Error('callback is not a function');
-    let context = this;
-    let result = [];
-
-    let O = Object(this);
-    let length = O.length;
-
-
-    if (thisArg) {
-        context = thisArg;
+function clear() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('lock');
     }
 
-    
-    for (let i = 0; i < length; i++) {
-        if (i in O) {
-            if (cb.call(context, this[i], i, O)){
-                result.push(this[i]);
-            }
+    solution.textContent = '';
+    operatorText.textContent = '';
+    isOperationClicked = false;
+    number1 = '';
+    number2 = '';
+    result = '';
+    isOperationClicked = false;
+    operator = '';
+}
+
+var countDecimals = function (value) {
+    if (Math.floor(value) !== value)
+        return value.toString().split(".")[1].length || 0;
+    return 0;
+}
+
+function addNumber(number) {
+
+    if (!isOperationClicked) {
+        if (number === '.' && number1.includes('.')) return;
+        number1 += number;
+        result = number1;
+
+    }
+    if (isOperationClicked) {
+        if (number === '.' && number2.includes('.')) return;
+        number2 += number;
+
+    }
+}
+
+function lock() {
+    if (solution.textContent == 'Error') {
+        for (let i = 0; i < buttons.length; i++) {
+            if (buttons[i].textContent !== 'AC') buttons[i].classList.add('lock');
         }
-        
+    };
+
+}
+
+
+function compute() {
+    if (number1 !== '' && number2 !== '') {
+        switch (operator) {
+            case '+':
+                result = +number1 + +number2;
+                number1 = result.toString();
+                number2 = '';
+                break;
+            case '-':
+                result = +number1 - +number2;
+                number1 = result.toString();
+                number2 = '';
+                break;
+            case 'x':
+                result = +number1 * +number2;
+                number1 = result.toString();
+                number2 = '';
+                break;
+            case '÷':
+                if (number2 == 0) {
+                    solution.textContent = 'Error';
+                    operatorText.textContent = '';
+                    lock();
+                }
+                else {
+                    result = number1 / number2;
+
+                    let remainder = Math.round((+result).toFixed(10).toString().slice(10) / 10);
+
+                    result = countDecimals(+result) > 8 ? (+result).toFixed(8) + remainder : +(result);
+                    number1 = result.toString();
+                    number2 = '';
+                }
+                break;
+
+            default:
+                break;
+        }
     }
-    
-    return result;
-
-    // while (i < this.length) {
-    //     if (i in this) {
-    //         cb.call(context, this[i], i, this);
-    //     }
-    //     i++;
-    // }
 
 
-};
+    if (event.target.textContent == '√' && number1 !== '') {
+        result = Math.sqrt(number1);
+        number1 = result.toString();
+        number2 = '';
+        solution.textContent = result;
+        operatorText.textContent = '';
 
-let arr = [1, 2, 3, 4, 5];
+    }
 
-let second = arr.myFilter((item,index,array) => item > 3);
-console.log(second);
+    if (event.target.textContent == '𝓍²' && number1 !== '') {
+        result = Math.pow(number1, 2);
+        number1 = result.toString();
+        number2 = '';
+        operatorText.textContent = '';
+        solution.textContent = result;
+
+    }
+}
+
+
+document.body.addEventListener('click', function (event) {
+    let target = event.target;
+
+    if (target.textContent == '+/-') {
+        if (!isOperationClicked) {
+            number1 = -number1;
+            result = number1;
+            solution.textContent = result;
+        }
+        if (isOperationClicked) {
+            number2 = -number2;
+            solution.textContent = result;
+        }
+    }
+
+
+
+    if (target.classList.contains('calc-button-digit')) {
+        addNumber(target.textContent);
+        if (!isOperationClicked) solution.textContent = number1;
+        if (isOperationClicked) solution.textContent = number2;
+    }
+
+
+    if (target.classList.contains('calc-button-operand')) {
+        isOperationClicked = true;
+
+        solution.textContent = '';
+
+        compute();
+        operator = target.textContent;
+        if (operator !== '√' && operator !== '𝓍²') operatorText.textContent = result + operator;
+    }
+
+    if (target.textContent == 'AC') clear();
+
+
+    if (target.textContent == '=') {
+        compute();
+        operatorText.textContent = '';
+        solution.textContent = result;
+    }
+
+    console.log('number1:', number1);
+
+    console.log('number2:', number2);
+    console.log('result: ', result);
+    console.log('operator', operator);
+
+
+    // console.log('b: ', b);
+    // console.log('bufer: ', bufer);
+    // // console.log('isOperationClicked', isOperationClicked);
+
+    // console.log('----------------------------------------------');
+});
